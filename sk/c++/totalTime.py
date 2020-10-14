6b30ff80c1b75f2f8e3259b3c5fa44db4e7d1a85
xihalameilin <1293086146@qq.com>
2020-10-14 19:57:19 +0800

    一般化

--- sk/c++/totalTime.py
+++ sk/c++/totalTime.py
@@ -1,19 +1,51 @@
 import pandas as pd
 import util
 import numpy as np
+import sys
+from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv(r"C:\Users\hp\PycharmProjects\2020winter\sk\c++\csv\totalTime.csv")
-userIds = np.array(data['userId'])
-data = data['totalTime']
-data = np.array(data).reshape(-1, 1)
+# [0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+#  0 1 1 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 1 1 0
+#  0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+#  1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+#  1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 1 0 1 0 0 0 0 0 0]
+
+
+# [0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+#  0 1 1 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 1 1 0
+#  0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+#  1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+#  1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 1 0 1 0 0 0 0 0 0]
+
+
+def getData(path):
+    ss = StandardScaler()
+    data = pd.read_csv(path)
+    data = data.dropna(axis=0, inplace=False)
+    userIds = np.array(data['userId'])
+    data = data.iloc[:, 1]
+    data = ss.fit_transform(np.array(data).reshape(-1, 1))
+    data = np.array(data).reshape(-1, 1)
+    n_cluster = util.get_best_n(data)
+    print(n_cluster)
+    print(userIds)
+    print(util.kmeans(n_cluster, data))
+
+
+if __name__ == '__main__':
+    # getData("r"+sys.argv[1])
+    getData(sys.argv[1])
 
 
 
 
-n_cluster = util.get_best_n(data)
-print(n_cluster)
-print(userIds)
-print(util.kmeans(n_cluster, data))
+
+
+
 
 
 
